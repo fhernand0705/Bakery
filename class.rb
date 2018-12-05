8c64d530fe7e72abc577695e0437c1fd0b8db220
Freddy Hernandez <freddy.hernandez@fandm.edu>
2018-12-05 16:41:12 -0500

    added navbar to layout file with styling

---
+++ class.rb
@@ -0,0 +1,41 @@
+# create your catagory classes here
+
+
+
+class Cookies
+
+    attr_accessor :name, :price, :description, :image
+    def initialize(name, price, description, image)
+           @name = name
+           @price = price
+           @despcrition = description
+           @image = image
+
+  end
+end
+
+
+class Muffins
+
+    attr_accessor :name, :price, :description, :image
+    def initialize(name, price, description, image)
+           @name = name
+           @price = price
+           @despcrition = description
+           @image = image
+
+  end
+end
+
+
+class Cupcakes
+
+    attr_accessor :name, :price, :description, :image
+    def initialize(name, price, description, image)
+           @name = name
+           @price = price
+           @despcrition = description
+           @image = image
+
+  end
+end
--- server.rb
+++ server.rb
@@ -1,7 +1,43 @@
 require 'sinatra'
+require '/class.rb'
 
 
-get '/' do
-  puts ENV['EMAIL_USERNAME']
-  erb :home
+get '/home' do
+
+   erb :home
 end
+
+get '/about us' do
+
+   erb :about_us
+end
+
+get '/cookies' do
+
+   erb :cookies
+end
+
+get '/muffins' do
+
+   erb :home
+end
+
+get '/cupcakes' do
+
+   erb :home
+end
+
+
+
+
+
+
+
+
+
+
+
+# get '/' do
+#   puts ENV['EMAIL_USERNAME']
+#   erb :home
+# end
