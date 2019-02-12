6931867050c75eaa802ea1502178ccc42ff511e6
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-02-12 15:51:04 -0500

    fixed some spacing for semantics

--- class.rb
+++ class.rb
@@ -1,41 +1,37 @@
-# create your catagory classes here
-
-
+# creates category classes
 class Cookie
 
-    attr_accessor :name, :price, :description, :image
-    def initialize(name, price, description, image)
-           @name = name
-           @price = price
-           @description = description
-           @image = image
-
-
-    end
+  attr_accessor :name, :price, :description, :image
+  def initialize(name, price, description, image)
+        @name = name
+        @price = price
+        @description = description
+        @image = image
+  end
 end
 
 
 class Muffin
 
-    attr_accessor :name, :price, :description, :image
-    def initialize(name, price, description, image)
-           @name = name
-           @price = price
-           @description = description
-           @image = image
+  attr_accessor :name, :price, :description, :image
+  def initialize(name, price, description, image)
+          @name = name
+          @price = price
+          @description = description
+          @image = image
 
-    end
+  end
 end
 
 
 class Cupcake
 
-    attr_accessor :name, :price, :description, :image
-    def initialize(name, price, description, image)
-           @name = name
-           @price = price
-           @description = description
-           @image = image
+  attr_accessor :name, :price, :description, :image
+  def initialize(name, price, description, image)
+          @name = name
+          @price = price
+          @description = description
+          @image = image
 
   end
 end
